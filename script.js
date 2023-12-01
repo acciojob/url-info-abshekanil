d730bf172f31eda0a19e57e7c9d07f39ec86697d
Abhishek Anil <141896050+abshekanil@users.noreply.github.com>
2023-12-01 21:09:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If required.
 const currentLocation = window.location.href;
 const lengthOfUrl = currentLocation.length;
-alert("The length of the URL is : "+ lengthOfUrl);
+alert("The length of the URL is: "+ lengthOfUrl); 
